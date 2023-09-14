df1b0a2c2e25432aa948f1d88b5b59edcc673931
Narottam Sarkar <130740563+Narottan123@users.noreply.github.com>
2023-09-14 16:30:45 +0530

    Update controller.js

--- controller/controller.js
+++ controller/controller.js
@@ -21,36 +21,8 @@ const saveTodo = async (req, res) => {
   }
 };
 
-const updateTodo = async (req, res) => {
-  try {
-    let { text, _id } = req.body;
-    let todo = await todomodel.findByIdAndUpdate(_id, {
-      $set: { text: text },
-    });
-
-    if (todo) {
-      res.send(todo);
-    } else {
-      res.status(404).send("Todo not found");
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Server error");
-  }
-};
-
-const deleteTodo = async (req, res) => {
-  try {
-    let { _id } = req.body;
-    let todo = await todomodel.findByIdAndDelete(_id);
-
-    if (todo) {
-      res.send("Deleted Successfully");
-    } else {
-      res.status(404).send("Todo not found");
-    }
-  } catch (error) {
-    console.error(error);
+module.exports = { getTodo, saveTodo };
+   
     res.status(500).send("Server error");
   }
 };
