48de71b77340270b05966495c7be3863de96ef37
Narottam Sarkar <130740563+Narottan123@users.noreply.github.com>
2023-09-14 16:29:26 +0530

    Update route.js

--- routes/route.js
+++ routes/route.js
@@ -4,6 +4,5 @@ const controller = require("../controller/controller");
 
 router.get("/", controller.getTodo);
 router.post("/save", controller.saveTodo);
-router.put("/update", controller.updateTodo);
-router.delete("/delete", controller.deleteTodo);
+
 module.exports = router;
